89c52d03147926e9a967c3d729536109b2bf2aa1
Winzum <winzum93@gmail.com>
2016-10-19 13:39:38 +0200

    Startup
    files toegevoegd

---
+++ Imperatief Programmeren - Reversi/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Imperatief_Programmeren___Reversi
+{
+    partial class Reversi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel = new System.Windows.Forms.Panel();
+            this.Help = new System.Windows.Forms.Button();
+            this.Nieuw = new System.Windows.Forms.Button();
+            this.Beurtbox = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.x_waarde = new System.Windows.Forms.TextBox();
+            this.y_waarde = new System.Windows.Forms.TextBox();
+            this.label_x = new System.Windows.Forms.Label();
+            this.label_y = new System.Windows.Forms.Label();
+            this.OK_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Panel
+            // 
+            this.Panel.Location = new System.Drawing.Point(52, 121);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(300, 300);
+            this.Panel.TabIndex = 0;
+            this.Panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Help
+            // 
+            this.Help.Location = new System.Drawing.Point(277, 38);
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(111, 32);
+            this.Help.TabIndex = 1;
+            this.Help.Text = "Help";
+            this.Help.UseVisualStyleBackColor = true;
+            this.Help.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Nieuw
+            // 
+            this.Nieuw.Location = new System.Drawing.Point(12, 37);
+            this.Nieuw.Name = "Nieuw";
+            this.Nieuw.Size = new System.Drawing.Size(112, 32);
+            this.Nieuw.TabIndex = 2;
+            this.Nieuw.Text = "Nieuw spel";
+            this.Nieuw.UseVisualStyleBackColor = true;
+            this.Nieuw.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Beurtbox
+            // 
+            this.Beurtbox.AutoSize = true;
+            this.Beurtbox.Location = new System.Drawing.Point(136, 40);
+            this.Beurtbox.Name = "Beurtbox";
+            this.Beurtbox.Size = new System.Drawing.Size(0, 13);
+            this.Beurtbox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // x_waarde
+            // 
+            this.x_waarde.Location = new System.Drawing.Point(199, 82);
+            this.x_waarde.Name = "x_waarde";
+            this.x_waarde.Size = new System.Drawing.Size(46, 20);
+            this.x_waarde.TabIndex = 5;
+            // 
+            // y_waarde
+            // 
+            this.y_waarde.Location = new System.Drawing.Point(304, 82);
+            this.y_waarde.Name = "y_waarde";
+            this.y_waarde.Size = new System.Drawing.Size(48, 20);
+            this.y_waarde.TabIndex = 6;
+            // 
+            // label_x
+            // 
+            this.label_x.AutoSize = true;
+            this.label_x.Location = new System.Drawing.Point(149, 85);
+            this.label_x.Name = "label_x";
+            this.label_x.Size = new System.Drawing.Size(35, 13);
+            this.label_x.TabIndex = 7;
+            this.label_x.Text = "label2";
+            // 
+            // label_y
+            // 
+            this.label_y.AutoSize = true;
+            this.label_y.Location = new System.Drawing.Point(251, 85);
+            this.label_y.Name = "label_y";
+            this.label_y.Size = new System.Drawing.Size(35, 13);
+            this.label_y.TabIndex = 8;
+            this.label_y.Text = "label2";
+            // 
+            // OK_button
+            // 
+            this.OK_button.Location = new System.Drawing.Point(373, 82);
+            this.OK_button.Name = "OK_button";
+            this.OK_button.Size = new System.Drawing.Size(41, 23);
+            this.OK_button.TabIndex = 9;
+            this.OK_button.Text = "OK";
+            this.OK_button.UseVisualStyleBackColor = true;
+            // 
+            // Reversi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 484);
+            this.Controls.Add(this.OK_button);
+            this.Controls.Add(this.label_y);
+            this.Controls.Add(this.label_x);
+            this.Controls.Add(this.y_waarde);
+            this.Controls.Add(this.x_waarde);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Beurtbox);
+            this.Controls.Add(this.Nieuw);
+            this.Controls.Add(this.Help);
+            this.Controls.Add(this.Panel);
+            this.Name = "Reversi";
+            this.Text = "Reversi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Panel;
+        private System.Windows.Forms.Button Help;
+        private System.Windows.Forms.Button Nieuw;
+        private System.Windows.Forms.Label Beurtbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox x_waarde;
+        private System.Windows.Forms.TextBox y_waarde;
+        private System.Windows.Forms.Label label_x;
+        private System.Windows.Forms.Label label_y;
+        private System.Windows.Forms.Button OK_button;
+    }
+}
+
---
+++ Imperatief Programmeren - Reversi/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Imperatief_Programmeren___Reversi
+{
+    public partial class Reversi : Form
+    {
+        //variabelen
+        int vakx, vaky;
+        int steen = 50;
+        int[,] speelveld;
+        int mousex, mousey;
+        int teller = 0;
+
+        //indeling veld
+        //0 is leeg
+        //1 is rood
+        //2 is blauw
+        //3 is hulp
+
+
+        public Reversi()
+        {
+            InitializeComponent();
+            this.Text = "Reversi";
+            vakx = 10;
+            vaky = 10;
+            Panel.Size = new Size(vakx * steen, vaky * steen);
+
+            //evenementen
+            Panel.MouseClick += klik;
+            OK_button.Click += button2_Click;
+            x_waarde.KeyDown += x_waarde_enter;
+            y_waarde.KeyDown += y_waarde_enter;
+
+            //array
+            speelveld = new int[vakx, vaky];
+            speelveld[vakx / 2 - 1, vaky / 2 - 1] = 2;
+            speelveld[vakx / 2, vaky / 2] = 2;
+            speelveld[vakx / 2, vaky / 2 - 1] = 1;
+            speelveld[vakx / 2 - 1, vaky / 2] = 1;
+
+            //aanpassen grootte speelveld - labels & tekstboxen
+            label1.Text = "Grootte van het speelveld:";
+            label_x.Text = "breedte:";
+            label_y.Text = "hoogte:";          
+
+        }
+
+
+
+        //Help button
+        private void button1_Click(object sender, EventArgs ea)
+        {
+            System.Windows.Forms.MessageBox.Show("Hier moeten instructies komen.");
+            
+        }
+
+        //Nieuw spel button
+        private void button2_Click(object sender, EventArgs ea)
+        {
+            //evt aanpassing van speelveld
+            veldWaarde();
+
+            //waardes resetten
+            for (int t = 0; t < vakx; t++)
+            {
+                for (int s = 0; s < vakx; s++)
+                {
+                    speelveld[t, s] = 0;
+                }
+            }
+
+            //beginwaardes meegeven
+            speelveld[vakx / 2 - 1, vaky / 2 - 1] = 2;
+            speelveld[vakx / 2, vaky / 2] = 2;
+            speelveld[vakx / 2, vaky / 2 - 1] = 1;
+            speelveld[vakx / 2 - 1, vaky / 2] = 1;
+            
+            //grootte speelveld aanpassen
+            Panel.Size = new Size(vakx * steen, vaky * steen);
+
+            Panel.Invalidate();
+        }
+
+        //indrukken ENTER na tekstbox x_waarde
+        public void x_waarde_enter(object obj, KeyEventArgs kea)
+        {
+            if (kea.KeyCode == Keys.Enter)
+                button2_Click(this, kea);
+        }
+
+        //indrukken ENTER na tekstbox y_waarde
+        public void y_waarde_enter(object obj, KeyEventArgs kea)
+        {
+            if (kea.KeyCode == Keys.Enter)
+                button2_Click(this, kea);
+        }
+
+
+        private void klik(object obj, MouseEventArgs mea)
+        {
+            mousex = mea.X / steen;
+            mousey = mea.Y / steen;
+
+            //beurt blauw-rood afwisselen
+            if (speelveld[mousex, mousey] != 1 && speelveld[mousex, mousey] != 2 && speelveld[mousex,mousey] == 3)
+            {
+                if (teller % 2 == 0)
+                {
+                    speelveld[mousex, mousey] = 2;
+                }
+                else
+                {
+                    speelveld[mousex, mousey] = 1;
+                }
+                teller += 1;
+            }
+
+            //array opschonen van tips
+            for (int t = 0; t < vakx; t++)
+            {
+                for (int s = 0; s < vakx; s++)
+                {
+                    if (speelveld[t, s] == 3)
+                    {
+                        speelveld[t, s] = 0;
+                    }
+                }
+            }
+            Panel.Invalidate();
+        }
+
+        private void veldWaarde()
+        {
+            int w_vakx, w_vaky;
+            int.TryParse(x_waarde.Text, out w_vakx);
+            int.TryParse(y_waarde.Text, out w_vaky);
+            if (w_vakx > 2)
+                vakx = w_vakx;
+            if (w_vaky > 2)
+                vaky = w_vaky;
+
+            speelveld = new int[vakx, vaky];
+            speelveld[vakx / 2 - 1, vaky / 2 - 1] = 2;
+            speelveld[vakx / 2, vaky / 2] = 2;
+            speelveld[vakx / 2, vaky / 2 - 1] = 1;
+            speelveld[vakx / 2 - 1, vaky / 2] = 1;
+
+            Panel.Invalidate();
+
+            //probleem: zodra vakx of vaky groter dan beginwaades ervan wordt gemaakt knalt ie eruit
+        }
+
+
+        private void steenHulp()
+        {
+            int kleur;
+            if (teller % 2 == 0)
+                kleur = 2;
+            else
+                kleur = 1;
+
+            for (int t = 0; t < vakx; t++)
+            {
+                for (int s = 0; s < vakx; s++)
+                {
+                    //kijken naar de stenen van de andere partij
+                    if (speelveld[t, s] != kleur && speelveld[t, s] != 0 && speelveld[t, s] != 3)
+                    {
+                        for(int x = -1; x<=1; x++)
+                        {
+                            for(int y = -1; y<= 1; y++)
+                            {
+                                //kijken in het raster rondom de steen van de tegenpartij + niet buiten speelveld vallen
+                                if(speelveld[t+x,s+y] != 1 && speelveld[t+x,s+y] != 2)
+                                {
+                                    if(speelveld[t+x*-1,s+y*-1] == kleur)
+                                    {
+                                        speelveld[t + x, s + y] = 3;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        //panel voor het paint event
+        private void panel1_Paint(object sender, PaintEventArgs pea)
+        {
+            this.steenHulp();
+                        
+            for (int t = 0; t <= vakx; t++)
+            {
+                for(int s = 0; s<= vaky; s++)
+                {
+                    pea.Graphics.DrawRectangle(Pens.Black, 0, 0, Panel.Width-1, Panel.Height-1);
+                    //horizontale lijnen
+                    pea.Graphics.DrawLine(Pens.Black, t * Panel.Width / vakx, 0, t * Panel.Width / vakx, Panel.Height);
+                    //verticale lijnen
+                    pea.Graphics.DrawLine(Pens.Black, 0, s * Panel.Height / vaky, Panel.Width, s * Panel.Height / vaky);
+                }
+            }
+            
+            for(int t = 0; t < vakx; t++)
+            {
+                for(int s = 0; s < vaky; s++)
+                {
+                    if(speelveld[t,s] != 0)
+                    {
+                        if(speelveld[t,s] == 1)
+                        {
+                            pea.Graphics.FillEllipse(Brushes.Red, t * Panel.Width / vakx, s * Panel.Height / vaky, steen, steen);
+                        }
+                        else if(speelveld[t,s] == 3)
+                        {
+                            pea.Graphics.DrawEllipse(Pens.Black, t * Panel.Width / vakx + 15, s * Panel.Height / vaky + 15, steen - 25, steen - 25);
+                        }
+                        else
+                        {
+                            pea.Graphics.FillEllipse(Brushes.Blue, t * Panel.Width / vakx, s * Panel.Height / vaky, steen, steen);
+                        }
+                    }
+                }
+            }
+
+            if (teller % 2 == 0)
+            {
+                Beurtbox.Text = "Blauw is aan de beurt.";
+            }
+            else
+            {
+                Beurtbox.Text = "Rood is aan de beurt.";
+            }
+        }
+    }
+}
