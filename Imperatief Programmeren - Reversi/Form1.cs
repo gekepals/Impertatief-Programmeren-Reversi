d1553f739a4f0daee67edde9f6db6b500cc64c2e
gekepals <gekehpals@gmail.com>
2016-10-23 11:38:14 +0200

    Poging tot verder kijken veldwaarde 3
    Poging tot schrijven van aparte methode 'LegaleZet' waarin gekeken wordt
    wat een legale zet is: uitbreiding van onze code waarin alleen gekeken
    wordt naar het -1, 0, 1 raster rondom een steen. NB: nog enkele
    problemen, aangegeven in de comments. Misschien dat een frisse blik
    helpt, ik kom er niet helemaal uit :)

--- Imperatief Programmeren - Reversi/Form1.cs
+++ Imperatief Programmeren - Reversi/Form1.cs
@@ -18,6 +18,7 @@ namespace Imperatief_Programmeren___Reversi
         int[,] speelveld;
         int mousex, mousey;
         int teller = 0;
+        int kleur;
 
         //indeling veld
         //0 is leeg
@@ -45,7 +46,7 @@ namespace Imperatief_Programmeren___Reversi
             //aanpassen grootte speelveld - labels & tekstboxen
             label1.Text = "Grootte van het speelveld:";
             label_x.Text = "breedte:";
-            label_y.Text = "hoogte:";          
+            label_y.Text = "hoogte:";
 
         }
 
@@ -55,7 +56,7 @@ namespace Imperatief_Programmeren___Reversi
         private void button1_Click(object sender, EventArgs ea)
         {
             System.Windows.Forms.MessageBox.Show("Hier moeten instructies komen.");
-            
+
         }
 
         //Nieuw spel button
@@ -63,7 +64,7 @@ namespace Imperatief_Programmeren___Reversi
         {
             //beginArray(vakx, vaky);
             //evt aanpassing van speelveld
-            
+
 
             //waardes resetten
             for (int t = 0; t < vakx; t++)
@@ -104,7 +105,7 @@ namespace Imperatief_Programmeren___Reversi
             mousey = mea.Y / steen;
 
             //beurt blauw-rood afwisselen
-            if (speelveld[mousex, mousey] != 1 && speelveld[mousex, mousey] != 2 && speelveld[mousex,mousey] == 3)
+            if (speelveld[mousex, mousey] != 1 && speelveld[mousex, mousey] != 2 && speelveld[mousex, mousey] == 3)
             {
                 if (teller % 2 == 0)
                 {
@@ -146,11 +147,11 @@ namespace Imperatief_Programmeren___Reversi
 
             Panel.Invalidate();
 
-            
+
         }
 
         //hier maken we de tweedimensionele array aan
-        private void  beginArray(int vakx, int vaky)
+        private void beginArray(int vakx, int vaky)
         {
             //Deze array is belangrijk
             speelveld = new int[vakx, vaky];
@@ -164,34 +165,84 @@ namespace Imperatief_Programmeren___Reversi
         //deze methode geeft de mogelijke plaatsen aan
         private void steenHulp()
         {
-            int kleur;
+            //int kleur;
             if (teller % 2 == 0)
                 kleur = 2;
             else
                 kleur = 1;
 
+            //het hele veld doorlopen
             for (int t = 0; t < vakx; t++)
             {
-                for (int s = 0; s < vakx; s++)
+                for (int s = 0; s < vaky; s++)
                 {
                     //kijken naar de stenen van de andere partij
                     if (speelveld[t, s] != kleur && speelveld[t, s] != 0 && speelveld[t, s] != 3)
                     {
-                        for(int x = -1; x<=1; x++)
+                        //legaleZet(t, s);
+                        //raster rondom steen bekijken: beginnen bij 1, ophogen indien nodig
+                        //probleem: hierbij nog bepaling dat ie niet buiten het veld eruit vliegt. Voor gemak heb ik nu 3 gekozen om deze code te testen
+                        for(int a = 1; a < 3; a++)
                         {
-                            for(int y = -1; y<= 1; y++)
+                            for(int b = 1; b < 3; b++)
                             {
-                                //kijken in het raster rondom de steen van de tegenpartij + niet buiten speelveld vallen
-                                if(speelveld[t+x,s+y] != 1 && speelveld[t+x,s+y] != 2)
+                                for (int x = -a; x <= a; x++)
                                 {
-                                    if(speelveld[t+x*-1,s+y*-1] == kleur)
+                                    for (int y = -b; y <= b; y++)
                                     {
-                                        speelveld[t + x, s + y] = 3;
+                                        //kijken in het raster rondom de steen van de tegenpartij + niet buiten speelveld vallen
+                                        if (speelveld[t + x, s + y] != 1 && speelveld[t + x, s + y] != 2 && t + x < vakx && t + x > 0 && s + y < vaky && s + y > 0)
+                                        {
+                                            if (t + x * -1 < vakx && t + x * -1 > 0 && s + y * -1 < vaky && s + y * -1 > 0)
+                                            {
+                                                if (speelveld[t + x * -1, s + y * -1] == kleur)
+                                                {
+                                                    speelveld[t + x, s + y] = 3;
+                                                    //probleem: code wordt te ver doorlopen, waardoor ook vakjes die niet grenzen aan een steen 3 worden
+                                                }
+                                            }
+                                        }
                                     }
                                 }
                             }
+                        }                                            
+                    }
+                }
+            }
+        }
+
+        //deze methode werkt nog niet goed
+        //idee is om een deel van steenhulp() in aparte methode te zetten zodat de code overzichtelijker wordt
+        //methode om te bepalen of iets een legale zet is
+        private void legaleZet(int t, int s)
+        {
+            //rondjes-schaal bepalen: begin bij 1, ophogen als nodig
+            for (int x = 1; x < 3; x++)
+            {
+                for (int y = 1; y < 3; y++)
+                {
+                    //schaal laten lopen door -1, 0 en 1
+                    for (int a = -x; a <= x; a++)
+                    {
+                        for (int b = -y; b <= y; b++)
+                        {
+                            //steen bepalen die niet kleur is en niet leeg --> vanuit daar kijken
+                            if (speelveld[t, s] != 1 && speelveld[t, s] != 2)
+                            {
+                                //poging tot niet uit laten vliegen bij randen
+                                //if (t + a * -1 > 0 && t + a * -1 < vakx && s + b * -1 > 0 && s + b * -1 < vaky)
+                                //{
+                                //bepalen of tegenovergestelde van veld de andere kleur is: dan geldige zet want insluiten
+                                if (speelveld[t + a * -1, s + b * -1] == kleur)
+                                {
+                                    //dan het veld 3 maken, aka doorzichtig rondje
+                                    speelveld[t + a, s + b] = 3;
+                                }
+                                //}
+                            }
                         }
                     }
+                
                 }
             }
         }
